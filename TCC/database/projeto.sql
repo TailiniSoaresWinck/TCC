1494c578c130fcd6947ae141585fc629857441e3
Tailini Soares Winck <tailini-swinck@educar.rs.gov.br>
2022-10-03 08:29:49 -0300

    Ajustando banco de dados

--- TCC/database/projeto.sql
+++ TCC/database/projeto.sql
@@ -1,3 +1,4 @@
+CREATE DATABASE projeto;
 use projeto;
 CREATE TABLE administrador(
 	id int not null auto_increment primary key,
@@ -36,6 +37,38 @@ CREATE TABLE empresa(
     senha varchar(50)
 );
 
+CREATE TABLE experiencia(
+	id int not null auto_increment primary key,
+    empresa varchar(50),
+    cargo varchar(50),
+    admissao date,
+    demissao date,
+    aluno_id int not null, 
+    foreign key(aluno_id) references aluno(id)
+);
+
+CREATE TABLE formacao(
+	id int not null auto_increment primary key,
+    instituicao varchar(50),
+    nivel varchar(20),
+    inicio date,
+    fim date,
+    curso_id int not null,
+    foreign key(curso_id) references curso(id),
+     aluno_id int not null, 
+    foreign key(aluno_id) references aluno(id)
+);
+
+CREATE TABLE curso_complementar(
+	id int not null auto_increment primary key,
+    curso varchar(50),
+    duracao int,
+    data date,
+	aluno_id int not null, 
+    foreign key(aluno_id) references aluno(id)
+);
+
+
 CREATE TABLE curriculo(
 	id int not null auto_increment primary key,
     area_profissional varchar(20),
@@ -50,40 +83,6 @@ CREATE TABLE curriculo(
     foreign key(curso_complementar_id) references curso_complementar(id)
 );
 
-CREATE TABLE experiencia(
-	id int not null auto_increment primary key,
-    empresa varchar(50),
-    cargo varchar(50),
-    admissao date,
-    demissao date
-);
-
-CREATE TABLE formacao(
-	id int not null auto_increment primary key,
-    instituicao varchar(50),
-    nivel varchar(20),
-    inicio date,
-    fim date,
-    curso_id int not null,
-    foreign key(curso_id) references curso(id)
-);
-
-CREATE TABLE curso_complementar(
-	id int not null auto_increment primary key,
-    curso varchar(50),
-    duracao int,
-    data date
-);
-
-CREATE TABLE historico_vaga(
-	id int not null auto_increment primary key,
-    curriculo_id int not null,
-    aluno_id int not null,
-    vaga_id int not null,
-    foreign key(vaga_id) references vaga(id),
-    foreign key(aluno_id) references aluno(id),
-    foreign key(curriculo_id) references curriculo(id)
-);
 
 CREATE TABLE vaga(
 	id int not null auto_increment primary key,
@@ -95,6 +94,17 @@ CREATE TABLE vaga(
     finalizado boolean,
     empresa_id int not null,
     foreign key(empresa_id) references empresa(id)
+);
+
+
+CREATE TABLE historico_vaga(
+	id int not null auto_increment primary key,
+    curriculo_id int not null,
+    aluno_id int not null,
+    vaga_id int not null,
+    foreign key(vaga_id) references vaga(id),
+    foreign key(aluno_id) references aluno(id),
+    foreign key(curriculo_id) references curriculo(id)
 );
 
 INSERT INTO curso(nome) VALUES (
@@ -121,4 +131,3 @@ INSERT INTO curso(nome) VALUES (
 INSERT INTO curso(nome) VALUES (
 	"Design de Móveis"
 );
-
