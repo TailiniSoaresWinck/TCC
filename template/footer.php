1b643ef8cfb1923e2a27a0711d2d54fd171c328b
Tailini Soares Winck <tailini-swinck@educar.rs.gov.br>
2022-10-04 09:14:14 -0300

    Delete template directory

--- template/footer.php
+++
@@ -1,7 +0,0 @@
-<html lang="pt-BR">
-<body>
-    <footer>
-        <p id="fp">PRATIKA CIMOL&copy2022</p>
-    </footer>
-</body>
-</html>
--- template/nav.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once('./cabecalho.php');
-?>
-<html lang="pt-BR">
-<body>
-</body>
-</html>
-<?php
-include_once('./footer.php');
