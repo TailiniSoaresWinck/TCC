ab0eb61df587e74cbcce3da4fce934677384e6f2
TailiniSoaresWinck <tailini-swinck@educar.rs.gov.br>
2022-11-14 16:44:48 -0300

    modified:   settings.php

--- settings.php
+++ settings.php
@@ -1,7 +1,7 @@
 <?php
 define("DIR_PATH", "C:\\xampp\htdocs\\TCC");
 define("TEMPLATE_PATH", DIR_PATH."/template");
-define("URL_BASE", "http://pratikacimol.herokuapp.com/");
+define("URL_BASE", "https://pratikacimol.herokuapp.com");
 define("URL_TEMPLATE", URL_BASE . "/template");
 define("URL_CSS", URL_BASE . "/css");
 define("URL_VIEWS", URL_BASE . "/views");
